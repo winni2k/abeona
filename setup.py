b79f08643f627ed389475d2c3633d5b00b95dba7
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-10-25 11:35:28 +0200

    Bump version: 0.34.4 → 0.34.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name='abeona',
-    version='0.34.4',
+    version='0.34.5',
     description='',
     long_description=readme,
     author='Warren W. Kretzschmar',
